be23fcc6f1e3e88a0eacdaecbca700cee83bb0dd
Keshav Rathor <35619632+keshav955@users.noreply.github.com>
2021-05-18 10:25:51 +0530

    Add files via upload

---
+++ even number/while loop even/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace while_loop_even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.while_loop();
+            p.do_while();
+            
+        }
+
+        void while_loop()
+        {
+
+            Console.Write("Please the enter a number : ");
+            int target = int.Parse(Console.ReadLine());
+
+            int start = 0;
+
+            while (start <= target)
+            {
+                Console.Write(start + " ");
+                start += 2;
+            }
+        }
+
+        void do_while()
+        {
+            string choice = " ";
+
+            do
+            {
+                Console.Write("Please the enter a number : ");
+                int target = int.Parse(Console.ReadLine());
+
+                int start = 0;
+
+                while (start <= target)
+                {
+                    Console.Write(start + " ");
+                    start += 2;
+                }
+
+                do
+                {
+                    Console.WriteLine("Do you Want to continue - Yes or No ");
+
+                    choice = Console.ReadLine().ToUpper();
+
+                    if (choice != "YES" && choice != "NO")
+                    {
+                        Console.WriteLine("Invalid Choice please Enter Yes or No");
+                    }
+                }
+                while (choice != "YES" && choice != "NO");
+            } while (choice == "YES");
+        }
+    }
+}
